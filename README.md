d0a31b6dc8bef2dab3640f7a79c4681a53a2930a
estherdalley <esther2.dalley@live.uwe.ac.uk>
2018-07-17 16:56:42 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Check Signal
+Check signal strength on iotbit
+
+## Description
+Check the signal strength of iotbit
+
+## Examples
+ - "How good is my signal?"
+ - "What's the signal strength here?"
+
+
+## Credits
+
+
+
---
+++ __init__.py
@@ -0,0 +1,46 @@
+from adapt.intent import IntentBuilder
+from mycroft import MycroftSkill, intent_file_handler
+from IOTBit_library_auto import Modem
+import math
+
+
+class CheckSignal(MycroftSkill):
+    def __init__(self):
+        MycroftSkill.__init__(self)
+        self.sigstr = "None"        # Human value
+        
+    @intent_file_handler('signal.check.intent')
+    def handle_signal_check(self, message):
+        # Set up modem, request signal value
+        APN = 'everywhere'          # YOUR APN HERE
+        _4G = Modem(APN,'4G')
+        _4G.sendATcmd('AT+CSQ',1000)
+
+        # Set numeric value - sometimes one digit.
+        if _4G.response[9].isdigit():
+            self.sig = int(_4G.response[8:9])
+        else:
+            self.sig = int(_4G.response[8])
+        # Get human-friendly value
+        if self.sig > 98:
+            self.sigstr = "None"
+        elif self.sig < 2:
+            self.sigstr = "Error: 0"
+        elif self.sig < 10:
+            self.sigstr = "Marginal"
+        elif self.sig < 15:
+            self.sigstr = "OK"
+        elif self.sig < 20:
+            self.sigstr = "Good"
+        elif self.sig > 19:
+            self.sigstr = "Excellent"
+        else:
+            self.sigstr = "Error: 1"
+            
+        self.speak_dialog('signal.check', data={"sigstr": self.sigstr})
+
+
+def create_skill():
+    return CheckSignal()
+
+
